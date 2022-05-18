fe07f336be57a632376e619f1387718af0de76a0
Josep Lleal <36154137+JosepLleal@users.noreply.github.com>
2022-05-18 21:54:59 +0200

    new character with new camera

---
+++ Replay System Project/Assets/ReplaySystem/Character/MovePlayer.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    public CharacterController controller;
+    public Animator animator;
+    public Transform cam;
+
+    public float gravity = -9.81f;
+    Vector3 velocity;
+
+    public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    public ReplayManager replay;
+
+
+    private void Update()
+    {
+        if (replay.ReplayMode())
+            return;
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            animator.SetBool("isWalking", true);
+
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, angle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+        if (controller.isGrounded)
+            velocity.y = 0f;
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
